86abd2de25133432692df63826f83f9275a6a1f1
yash-patel04 <yashpate04@gmail.com>
2025-03-06 10:58:58 +0530

    check

--- Server.js
+++ Server.js
@@ -32,12 +32,10 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error('Not allowed by CORS'));
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
