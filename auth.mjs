ef438994749536bc5428a1a51248f63f7fb6fbb9
yash-patel04 <yashpate04@gmail.com>
2025-03-08 17:53:46 +0530

    connected

--- auth.mjs
+++ auth.mjs
@@ -153,17 +153,17 @@ const createRouter = ({ UserModel, CategoryModel }) => {
   });
 
   // Connection Message for React
-  // router.get("/mqtt/connected", async (req, res) => {
-  //   try {
-  //     const msg = getConnection();
-  //     if (!msg) {
-  //       return res.status(404).json({ message: "No connection message set" });
-  //     }
-  //     res.status(200).json({ message: msg });
-  //   } catch (err) {
-  //     res.status(500).json({ message: "Internal server error" });
-  //   }
-  // });
+  router.get("/mqtt/connected", async (req, res) => {
+    try {
+      const msg = getConnection();
+      if (!msg) {
+        return res.status(404).json({ message: "No connection message set" });
+      }
+      res.status(200).json({ message: msg });
+    } catch (err) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
 
   return router;
 };
